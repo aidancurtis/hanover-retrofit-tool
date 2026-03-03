ad7a210f297507cacc10fa47bbd8a955ce645c55
aidancurtis <aicurtis@pm.me>
2026-03-03 12:25:12 -0500

    fix title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { RouterProvider } from "react-router";
-import { useEffect } from "react";
 import { router } from "./routes";
 
 function App() {
-    useEffect(() => {
-        document.title = "Hanover Retrofit Tool";
-    }, []);
-
     return <RouterProvider router={router} />;
 }
 
